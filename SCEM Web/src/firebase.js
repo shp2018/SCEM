4e7d76b0eca51fcb076ca152bb1dee2225268dcd
tomho5 <tungquangho@gmail.com>
2023-04-04 01:21:38 -0400

    linked authentication with firestore

--- SCEM Web/src/firebase.js
+++ SCEM Web/src/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClVMzqHeGaGmWCFLcVD0aS450TKKgRhVk",
-  authDomain: "scem-1dec5.firebaseapp.com",
-  projectId: "scem-1dec5",
-  storageBucket: "scem-1dec5.appspot.com",
-  messagingSenderId: "497821850518",
-  appId: "1:497821850518:web:801b1501b984370a649bb7"
+  apiKey: "AIzaSyAgEgiA2vB6Z4xV0cYdvXxtWkooDq1qGUo",
+  authDomain: "scem-ef60b.firebaseapp.com",
+  projectId: "scem-ef60b",
+  storageBucket: "scem-ef60b.appspot.com",
+  messagingSenderId: "915273599876",
+  appId: "1:915273599876:web:9ebf23d09527ca2c65ebd1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
+export const auth = getAuth(app);
