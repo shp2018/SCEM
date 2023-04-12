a4f1e9be84f43536d98404de5dd6a3c5979f2295
Kevin Park <shp291800@gmail.com>
2023-04-12 16:40:06 -0700

    marketplace draft

--- SCEM Web/src/App.js
+++ SCEM Web/src/App.js
@@ -6,16 +6,19 @@ import Login from './routes/login';
 import Home from './routes/home';
 import ResetPassword from './routes/resetPw';
 import ForgotPassword from './routes/forgot';
+import Post from './routes/post'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
   <Router>
     <Routes>
-      <Route exact path="/" exact element={<Home/>} />
+      <Route exact path="/" element={<Home/>} />
       <Route path="/login"  element={<Login/>} />
       <Route path="/signup" element={<Signup/>} />
       <Route path="/forgotPassword" element={<ForgotPassword/>} />
       <Route path="/resetPassword" element={<ResetPassword/>} />
+      <Route path="/post" element={<Post/>} />
       </Routes>
   </Router>
   );
