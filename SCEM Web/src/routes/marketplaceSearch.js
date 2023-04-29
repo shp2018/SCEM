06559e387925036a24f1f1acc87706735a81ac76
Ben N <pg.benng@gmail.com>
2023-04-29 15:18:40 -0700

    added variables for input boxes

--- SCEM Web/src/routes/marketplaceSearch.js
+++ SCEM Web/src/routes/marketplaceSearch.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import '../css/marketplaceSearch.css';
 
 function MarketplaceSearch() {
+    const [name, setName] = useState("");
+    const [equipmentType, setEquipmentType] = useState("");
+    const [site, setSite] = useState("");
+    const [description, setDescription] = useState("");
+    const [fromDate, setFromDate] = useState("");
+    const [toDate, setToDate] = useState("");
+
+    function setState() {
+        setName(document.getElementById("marketplaceSearch-nameInput").value);
+        setEquipmentType(document.getElementById("marketplaceSearch-equipmentTypeInput").value);
+        setSite(document.getElementById("marketplaceSearch-siteInput").value);
+        setDescription(document.getElementById("marketplaceSearch-descriptionInput").value);
+        setFromDate(document.getElementById("marketplaceSearch-fromDateInput").value);
+        setToDate(document.getElementById("marketplaceSearch-toDateInput").value);
+    }
 
     const search = e => {
         e.preventDefault();
+
+
     }
 
     return (
@@ -17,36 +34,36 @@ function MarketplaceSearch() {
             <form onSubmit={search}>
                 <div id={"marketplaceSearch-inputName"}>
                     <p id={"marketplaceSearch-subtitles"}> Name </p>
-                    <input type={"text"}/>
+                    <input type={"text"} id={"marketplaceSearch-nameInput"}/>
                 </div>
                 <div id={"marketplaceSearch-equipmentType"}>
                     <p id={"marketplaceSearch-subtitles"}> Equipment Type </p>
-                    <select>
-                        <option> All</option>
+                    <select id={"marketplaceSearch-equipmentTypeInput"}>
+                        <option> All </option>
                     </select>
                 </div>
                 <div id={"marketplaceSearch-site"}>
                     <p id={"marketplaceSearch-subtitles"}> Site </p>
-                    <select>
-                        <option> All</option>
+                    <select id={"marketplaceSearch-siteInput"}>
+                        <option> All </option>
                     </select>
                 </div>
                 <div id={"marketplaceSearch-description"}>
                     <p id={"marketplaceSearch-subtitles"}> Description </p>
-                    <input type={"text"}/>
+                    <input type={"text"} id={"marketplaceSearch-descriptionInput"}/>
                 </div>
                 <div id={"marketplaceSearch-fromDateToDate"}>
                     <div id={"marketplaceSearch-fromDate"}>
                         <p id={"marketplaceSearch-subtitles"}> From Date </p>
-                        <input type={"date"}/>
+                        <input type={"date"} id={"marketplaceSearch-fromDateInput"}/>
                     </div>
                     <div id={"marketplaceSearch-toDate"}>
                         <p id={"marketplaceSearch-subtitles"}> To Date </p>
-                        <input type={"date"}/>
+                        <input type={"date"} id={"marketplaceSearch-toDateInput"}/>
                     </div>
                 </div>
                 <div id={"marketplaceSearch-search"}>
-                    <button type={"submit"} id={"marketplaceSearch-searchButton"}> Search</button>
+                    <button type={"submit"} id={"marketplaceSearch-searchButton"} onClick={setState}> Search</button>
                 </div>
             </form>
         </div>
