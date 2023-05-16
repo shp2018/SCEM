7c35f1d3bc13e5746774dce9ae8633c43755bdc8
Ben N <pg.benng@gmail.com>
2023-05-16 17:21:44 +0700

    added HTML + CSS header elements to route js file

--- SCEM Web/src/routes/marketplace.js
+++ SCEM Web/src/routes/marketplace.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import {collection, getDocs} from "@firebase/firestore";
 import {firestore} from "../firebase";
 import {query, where} from "firebase/firestore";
+import '../css/marketplace.css';
 
 function Marketplace() {
     const [marketplaceItems, setMarketplaceItems] = useState([]);
@@ -22,12 +23,26 @@ function Marketplace() {
 
     return (
         <div>
-            <center>
-                <h3> Marketplace </h3>
-                <div id="marketplaceItems">
-                    {marketplaceItems}
-                </div>
-            </center>
+            <div id={"marketplace-header"}>
+                <img src={"logoWithBackground.jpg"}
+                     id={"marketplace-SCEMLogo"}
+                     alt={"SCEM Logo"}>
+                </img>
+
+                <a href={"/marketplace/search"}
+                   id={"marketplace-searchButton"}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                         className="bi bi-search" viewBox="0 0 16 16">
+                        <path
+                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </a>
+            </div>
+
+            <div id="marketplace-marketplaceItems">
+                {marketplaceItems}
+            </div>
+
         </div>
 
     );
