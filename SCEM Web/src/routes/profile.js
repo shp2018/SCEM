84c20a30634f0e1b0e88061dfb53740dd668a744
Benjamin Nguyen <pg.benng@gmail.com>
2023-06-02 13:28:41 -0700

    add profile route

--- SCEM Web/src/routes/profile.js
+++ SCEM Web/src/routes/profile.js
@@ -1,42 +1,28 @@
-import React, {useState} from "react";
-import {user} from "./login";
+import React, {useEffect, useState} from "react";
 import '../css/profile.css';
-import {query, where} from "firebase/firestore";
-import {collection, getDocs} from "@firebase/firestore";
+import {collection} from "firebase/firestore";
 import {firestore} from "../firebase";
 
 function Profile() {
     const [userPastRentals, setUserPastRentals] = useState([]);
-    const ref = collection(firestore, "marketplace");
+    const marketplaceRef = collection(firestore, "marketplace");
 
     async function getUserPastRentals() {
-        // TODO: query for items that match user fullname
-        const q = query(ref, where("name", "!=", ""));
-        const querySnapshot = await getDocs(q);
-        setUserPastRentals([]);
 
-        querySnapshot.forEach((doc) => {
-            let data = doc.data();
-            console.log(data);
-            setUserPastRentals(curr => [...curr,
-                // TODO: change structure
-                <div id={"profile-userPastRentalItem"}>
-                    <div> {data.name} </div>
-                    <div> {data.description} </div>
-                </div>]);
-        });
     }
 
-    getUserPastRentals()
+    useEffect(() => {
+        getUserPastRentals();
+    }, []);
 
     return (
         <div>
             <div id={"profile-header"}>
                 <img src={"logoWithBackground.jpg"}
-                     id={"profile-headerImage"}></img>
-                {/*{user}*/}
+                     id={"profile-headerImage"}
+                     alt={"SCEM Logo"}></img>
             </div>
-
+            Profile
             <div id={"profile-userPastRentals"}>
                 {userPastRentals}
             </div>
