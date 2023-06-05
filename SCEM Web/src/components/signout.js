2a1cfd5690c032ad2980fc2e30de95f63b6535b1
Benjamin Nguyen <pg.benng@gmail.com>
2023-06-04 22:25:58 -0700

    final - added signout to home page

--- SCEM Web/src/components/signout.js
+++ SCEM Web/src/components/signout.js
@@ -1,10 +1,21 @@
 import React from "react";
+import {auth} from "../firebase";
+import '../css/signout.css';
 
 function SignOut() {
+    const removeAuthentication = (e) => {
+        e.preventDefault();
+
+        auth.signOut().then(() => {
+            window.location.reload();
+        })
+    }
 
     return (
-        <div>
-
+        <div id={"signOut"}>
+            <button onClick={removeAuthentication}
+                    id={"signOut-signOutButton"}> Sign Out
+            </button>
         </div>
     );
 
--- SCEM Web/src/routes/home.js
+++ SCEM Web/src/routes/home.js
@@ -1,29 +1,38 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import HomeWithoutLogin from "../components/homeWithoutLogin";
 import HomeWithLogin from "../components/homeWithLogin";
 import {onAuthStateChanged} from "firebase/auth";
 import {auth} from "../firebase";
 
 function Home() {
-    const [authState, setAuthState] = useState(false);
+    const [authState, setAuthState] = useState(null);
 
     function checkAuthState() {
         onAuthStateChanged(auth, (user) => {
             if (user) {
                 setAuthState(true);
+            } else {
+                setAuthState(false);
             }
         })
     }
 
-    checkAuthState();
+    useEffect(() => {
+        checkAuthState();
+        // eslint-disable-next-line
+    }, []);
 
-    if (authState) {
+    if (authState === true) {
         return (
             <HomeWithLogin> </HomeWithLogin>
         );
-    } else {
+    } else if (authState === false) {
         return (
             <HomeWithoutLogin> </HomeWithoutLogin>
+        );
+    } else if (authState === null) {
+        return (
+          <div> Loading... </div>
         );
     }
 
