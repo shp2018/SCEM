b8992d7b70d418fc260fde1404d3c77180d0115b
Benjamin Nguyen <pg.benng@gmail.com>
2023-06-17 12:22:28 -0700

    modified account type input

--- SCEM Web/src/routes/companyProfile.js
+++ SCEM Web/src/routes/companyProfile.js
@@ -76,80 +76,84 @@ function CompanyProfile() {
     }
 
     return (<body>
-        <div id={"companyProfile-page"}>
-            <div id={"companyProfile-header"}>
-                <div id={"companyProfile-backButton"}>
-                    <a href={"/"}
-                       className={"arrow left"}>
-                    </a>
-                </div>
-                <div id={"companyProfile-companyProfileText"}>
-                    <h3> Company Profile Management </h3>
-                </div>
+    <div id={"companyProfile-page"}>
+        <div id={"companyProfile-header"}>
+            <div id={"companyProfile-backButton"}>
+                <a href={"/"}
+                   className={"arrow left"}>
+                </a>
             </div>
-            <br></br>
-            <form id="companyProfile-form" onSubmit={handleSave}>
-                <div>
-                    <div id={"companyProfile-inputBoxes"}>
-                        <label id="companyProfile-Label">Company Name</label>
-                        <input id="companyProfile-Input" type="text" placeholder="Company name" onChange={(e) => {
-                            setcName(e.target.value)
-                        }}></input><br></br>
-
-                        <label id="companyProfile-Label">Company Email</label>
-                        <input id="companyProfile-Input" type="text" placeholder="Company email"
-                               onChange={(e) => {
-                                   setcEmail(e.target.value)
-                               }}></input><br></br>
-
-                        <label id="companyProfile-Label">Company Phone Number</label>
-                        <input id="companyProfile-Input" type="text" placeholder="Company phone number"
-                               onChange={(e) => {
-                                   setcNumber(e.target.value)
-                               }}></input><br></br>
-
-                        <label id="companyProfile-Label">Company Owner</label>
-                        <input id="companyProfile-Input" type="text" placeholder="Company owner"
-                               onChange={(e) => {
-                                   setcOwner(e.target.value)
-                               }}></input><br></br>
-
-                        <label id="companyProfile-Label">Technical Support Email</label>
-                        <input id="companyProfile-Input" type="text" placeholder="Technical support email"
-                               onChange={(e) => {
-                                   settEmail(e.target.value)
-                               }}></input><br></br>
-
-                        <label id="companyProfile-Label">Technical Support Phone</label>
-                        <input id="companyProfile-Input" type="text" placeholder="Technical support phone"
-                               onChange={(e) => {
-                                   settNumber(e.target.value)
-                               }}></input><br></br>
-
-                        <label id="companyProfile-Label">Technical Person Name </label>
-                        <input id="companyProfile-Input" type="text" placeholder="Technical person name"
-                               onChange={(e) => {
-                                   settName(e.target.value)
-                               }}></input><br></br>
-
-                        <label id="companyProfile-Label">Tax ID</label><br></br>
-                        <input id="companyProfile-Input" type="text" placeholder="Tax ID"
-                               onChange={(e) => {
-                                   settaxID(e.target.value)
-                               }}></input><br></br>
-                        <br></br>
-
-                        <label id="companyProfile-Label">Account Type</label><br></br>
-                        <input id="companyProfile-Input" type="text" placeholder="Account type" onChange={(e) => {
-                            setaccType(e.target.value)
-                        }}></input><br></br>
-                    </div>
-
-                    <button id="companyProfile-companyProfilebutton" type="submit">Update</button>
-                </div>
-            </form>
+            <div id={"companyProfile-companyProfileText"}>
+                <h3> Company Profile Management </h3>
+            </div>
         </div>
-        </body>)
+        <br></br>
+        <form id="companyProfile-form" onSubmit={handleSave}>
+            <div>
+                <div id={"companyProfile-inputBoxes"}>
+                    <label id="companyProfile-Label">Company Name</label>
+                    <input id="companyProfile-Input" type="text" placeholder="Company name" onChange={(e) => {
+                        setcName(e.target.value)
+                    }}></input><br></br>
+
+                    <label id="companyProfile-Label">Company Email</label>
+                    <input id="companyProfile-Input" type="text" placeholder="Company email"
+                           onChange={(e) => {
+                               setcEmail(e.target.value)
+                           }}></input><br></br>
+
+                    <label id="companyProfile-Label">Company Phone Number</label>
+                    <input id="companyProfile-Input" type="text" placeholder="Company phone number"
+                           onChange={(e) => {
+                               setcNumber(e.target.value)
+                           }}></input><br></br>
+
+                    <label id="companyProfile-Label">Company Owner</label>
+                    <input id="companyProfile-Input" type="text" placeholder="Company owner"
+                           onChange={(e) => {
+                               setcOwner(e.target.value)
+                           }}></input><br></br>
+
+                    <label id="companyProfile-Label">Technical Support Email</label>
+                    <input id="companyProfile-Input" type="text" placeholder="Technical support email"
+                           onChange={(e) => {
+                               settEmail(e.target.value)
+                           }}></input><br></br>
+
+                    <label id="companyProfile-Label">Technical Support Phone</label>
+                    <input id="companyProfile-Input" type="text" placeholder="Technical support phone"
+                           onChange={(e) => {
+                               settNumber(e.target.value)
+                           }}></input><br></br>
+
+                    <label id="companyProfile-Label">Technical Person Name </label>
+                    <input id="companyProfile-Input" type="text" placeholder="Technical person name"
+                           onChange={(e) => {
+                               settName(e.target.value)
+                           }}></input><br></br>
+
+                    <label id="companyProfile-Label">Tax ID</label><br></br>
+                    <input id="companyProfile-Input" type="text" placeholder="Tax ID"
+                           onChange={(e) => {
+                               settaxID(e.target.value)
+                           }}></input><br></br>
+                    <br></br>
+
+                    <label id="companyProfile-Label">Account Type</label><br></br>
+                    <select id="companyProfile-Input" placeholder="Account type" defaultValue={"User"}
+                            onChange={(e) => {
+                                setaccType(e.target.value)
+                            }}>
+                        <option>User</option>
+                        <option>Company</option>
+                    </select><br></br>
+                </div>
+
+                <button id="companyProfile-companyProfilebutton" type="submit">Update</button>
+            </div>
+        </form>
+    </div>
+    </body>)
 }
 
 export default CompanyProfile;
