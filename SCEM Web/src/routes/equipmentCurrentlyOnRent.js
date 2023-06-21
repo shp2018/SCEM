fe13a91a22185a8f03b7e5072f03524b02630d1b
Benjamin Nguyen <pg.benng@gmail.com>
2023-06-21 11:47:59 -0700

    final - 12

--- SCEM Web/src/routes/equipmentCurrentlyOnRent.js
+++ SCEM Web/src/routes/equipmentCurrentlyOnRent.js
@@ -1,120 +1,101 @@
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import {
-  doc,
-  getDoc,
-  updateDoc,
-  collection,
-  query,
-  where,
-  getDocs,
-} from "firebase/firestore";
-import { auth, firestore } from "../firebase";
+import React, {useEffect, useState} from "react";
+import {collection, query, where, getDocs} from "firebase/firestore";
+import {auth, firestore} from "../firebase";
 import "../css/marketplaceItem.css";
-import { onAuthStateChanged } from "firebase/auth";
+import {onAuthStateChanged} from "firebase/auth";
 import "../css/equipmentCurrentlyOnRent.css";
 
 function EquipmentCurrentlyOnRent() {
-  const location = useLocation();
-  const [itemData, setItemData] = useState(null);
-  const [userID, setUserID] = useState("");
-  const [userName, setUserName] = useState("");
-  const [authState, setAuthState] = useState(false);
+    const [userID, setUserID] = useState("");
+    const [userName, setUserName] = useState("");
 
-  const [marketplaceItems, setMarketplaceItems] = useState([]);
-  const marketplaceRef = collection(firestore, "marketplace");
+    const [marketplaceItems, setMarketplaceItems] = useState([]);
+    const marketplaceRef = collection(firestore, "marketplace");
 
-  useEffect(() => {
-    const checkAuthState = async () => {
-      onAuthStateChanged(auth, async (user) => {
-        if (user) {
-          setUserID(user.uid);
-          setAuthState(true);
-          const ref = collection(firestore, "users");
-          const q = query(ref, where("email", "==", user.email));
-          const querySnapshot = await getDocs(q);
-          querySnapshot.forEach((doc) => {
-            if (!userName) {
-              setUserName(doc.data().fullname);
-            }
-          });
+    useEffect(() => {
+        const checkAuthState = async () => {
+            onAuthStateChanged(auth, async (user) => {
+                if (user) {
+                    setUserID(user.uid);
+                    const ref = collection(firestore, "users");
+                    const q = query(ref, where("email", "==", user.email));
+                    const querySnapshot = await getDocs(q);
+                    querySnapshot.forEach((doc) => {
+                        if (!userName) {
+                            setUserName(doc.data().fullname);
+                        }
+                    });
+                }
+            });
+        };
+        checkAuthState();
+    }, [userName]);
+
+    useEffect(() => {
+        if (userID) {
+            getMarketplaceData();
         }
-      });
-    };
+    }, [userID]);
 
-    checkAuthState();
-  }, [userName]);
+    async function getMarketplaceData() {
+        const marketplaceQuery = query(marketplaceRef, where("onRentID", "==", userID));
+        const querySnapshot = await getDocs(marketplaceQuery);
 
-  useEffect(() => {
-    if (userID) {
-      getMarketplaceData();
+        const items = querySnapshot.docs.map(doc => {
+            const data = doc.data();
+
+            return (
+                <tr key={`${doc.id}`}>
+                    <td className={"equipmentCurrentlyOnRent-tableElement"}></td>
+                    <td className={"equipmentCurrentlyOnRent-tableElement"}>{data.name}</td>
+                    <td className={"equipmentCurrentlyOnRent-tableElement"}>{data.dailyPrice}$</td>
+                    <td className={"equipmentCurrentlyOnRent-tableElement"}>{data.onRentName}</td>
+                    <td className={"equipmentCurrentlyOnRent-tableElement"}>
+                        <a href={`/marketplace/${doc.id}`}>
+                            <img src={"/triangle-right.svg"} alt={"Right arrow used to redirect user to item link."}
+                                 id={"equipmentCurrentlyOnRent-tableLinkArrow"}></img>
+                        </a>
+                    </td>
+                </tr>
+            );
+        });
+        setMarketplaceItems(items);
     }
-  }, [userID]);
 
-  async function getMarketplaceData() {
-    const marketplaceQuery = query(marketplaceRef, where("onRentID", "==", userID));
-    const querySnapshot = await getDocs(marketplaceQuery);
-    
-    const items = querySnapshot.docs.map((doc, index) => {
-      const data = doc.data();
-  
-      return (
-        <tr key={doc.id}>
-          <td></td>
-          <td>{data.name}</td>
-          <td>{data.dailyPrice}$</td>
-          <td>{data.onRentName}</td>
-          <td>
-            <button
-              className="triangle-button"
-              onClick={() => handleRedirectToDetails(doc.id)}
-            >
-            </button>
-          </td>
-        </tr>
-      );
-    });
-  
-    setMarketplaceItems(items);
-  }
-  
-  
-  
-  
+    useEffect(() => {
+        getMarketplaceData();
+        // eslint-disable-next-line
+    }, []);
 
-  useEffect(() => {
-    getMarketplaceData();
-    // eslint-disable-next-line
-  }, []);
-
-  const handleRedirectToDetails = (docId) => {
-    const url = `http://localhost:3000/marketplace/${docId}`;
-    window.location.href = url;
-  };
-
-  return (
-    <body id="equipmentCurrentlyOnRent-body">
-      <div id="blueBox">
-        <h1>Equipment currently on rent</h1>
-      </div>
-      {marketplaceItems.length > 0 ? (
-        <table id="marketplaceTable">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th>Renter</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>{marketplaceItems}</tbody>
-        </table>
-      ) : (
-        <p>No items currently on rent.</p>
-      )}
-    </body>
-  );
+    return (
+        <div id="equipmentCurrentlyOnRent-body">
+            <div id="equipmentCurrentlyOnRent-header">
+                <div id={"equipmentCurrentlyOnRent-backButton"}>
+                    <a href={"/"}
+                       className={"arrow left"}>
+                    </a>
+                </div>
+                <h3 id={"equipmentCurrentlyOnRent-titleText"}>Equipment currently on rent</h3>
+            </div>
+            {marketplaceItems.length > 0 ? (
+                <table className={"equipmentCurrentlyOnRent-tableElement"}
+                id={"equipmentCurrentlyOnRent-table"}>
+                    <tbody>
+                    <tr>
+                        <th className={"equipmentCurrentlyOnRent-tableHeading"}>#</th>
+                        <th className={"equipmentCurrentlyOnRent-tableHeading"}>Name</th>
+                        <th className={"equipmentCurrentlyOnRent-tableHeading"}>Price</th>
+                        <th className={"equipmentCurrentlyOnRent-tableHeading"}>Renter</th>
+                        <th className={"equipmentCurrentlyOnRent-tableHeading"}></th>
+                    </tr>
+                    </tbody>
+                    {marketplaceItems}
+                </table>
+            ) : (
+                <p className={"loading"}>Loading...</p>
+            )}
+        </div>
+    );
 }
 
 export default EquipmentCurrentlyOnRent;
