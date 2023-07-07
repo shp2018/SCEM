cffae37e08bd25ce8c3392159f5a3a4122546a7d
Benjamin Nguyen <pg.benng@gmail.com>
2023-07-07 11:05:00 -0700

    temp

--- SCEM Web/src/components/googleLogin.js
+++ SCEM Web/src/components/googleLogin.js
@@ -1,44 +1,38 @@
 import {GoogleLogin} from 'react-google-login';
 import React from "react";
-import { firestore } from "../firebase";
-import { doc, setDoc } from "@firebase/firestore";
+import {firestore} from "../firebase";
+import {doc, setDoc} from "firebase/firestore";
 
-const clientId = "1020057730481-3iflk45qqttk0v8pg48bjk4j0nmi6qm2.apps.googleusercontent.com"
+const clientId = "1020057730481-3iflk45qqttk0v8pg48bjk4j0nmi6qm2.apps.googleusercontent.com";
 
-function Glogin(){
-    const onSuccess= (res)=>{
-        const ref = doc(firestore,"users",res.profileObj.googleId);
-      let data = {
-        fullname:res.profileObj.name,
-        email:res.profileObj.email,
-        locked:false
-      }
-      try {
-        setDoc(ref,data)
-      } catch (e) {
-        console.log(e);
-      }
+function Glogin() {
+    const onSuccess = res => {
+        const ref = doc(firestore, "users", res.profileObj.googleId);
+        let data = {
+            fullname: res.profileObj.name,
+            email: res.profileObj.email,
+            locked: false,
+        }
+        try {
+            setDoc(ref, data);
+        } catch (e) {
+            console.log(e);
+        }
     }
-    const onFailure= (res)=>{
-        console.log("LOGIN FAILED! res: ",res)
+    const onFailure = res => {
+        console.log("LOGIN FAILED! res: ", res);
     }
-    return(
-
-       
-            <GoogleLogin
-            clientId = {clientId}
-            buttonText = "Sign in with Google"
-            onSuccess = {onSuccess}
-            onFailure = {onFailure}
+    return (
+        <GoogleLogin
+            clientId={clientId}
+            buttonText="Sign in with Google"
+            onSuccess={onSuccess}
+            onFailure={onFailure}
             cookiePolicy={'single_host_origin'}
-            isSignedIn = {true}
+            isSignedIn={true}
             className="btnGoogle"
-            ></GoogleLogin>
-
-   
-        
-
-
-   )
+        ></GoogleLogin>
+    )
 }
+
 export default Glogin;
