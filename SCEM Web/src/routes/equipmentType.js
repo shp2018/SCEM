7e80a9c88358631ad9fe82146e0fdd6a1f0ab9f0
Benjamin Nguyen <pg.benng@gmail.com>
2023-07-06 18:31:36 -0700

    final - 20

--- SCEM Web/src/routes/equipmentType.js
+++ SCEM Web/src/routes/equipmentType.js
@@ -1,47 +1,42 @@
 import React, {useState, useEffect} from 'react';
-import {getDocs, query, collection} from "firebase/firestore";
+import {getDocs, query, collection, doc, deleteDoc, updateDoc} from "firebase/firestore";
 import '../css/equipmentType.css';
 import {firestore} from "../firebase";
 
 const EquipmentType = () => {
     const [equipmentTypesData, setEquipmentTypesData] = useState([]);
     const [loaded, setLoaded] = useState(false);
+    const [editing, setEditing] = useState([]);
 
-    const dropdown = () => {
-        document.getElementById("myDropdown").classList.toggle("show");
-    }
-    window.onclick = (event) => {
-        if (event.target.matches('.equipmentType-dropdownButton')) {
-            const dropdowns = document.getElementsByClassName("dropdown-content");
-            for (let i = 0; i < dropdowns.length; i++) {
-                if (dropdowns[i].classList.contains('show')) {
-                    dropdowns[i].classList.remove('show');
-                }
-            }
-        }
+    const handleEdit = idx => {
+        const mutatedEditing = editing.map((element, index) => {
+            if (index === idx) return true;
+            else return element;
+        });
+        setEditing(mutatedEditing);
     }
 
-    // TODO: these 2 functions
-    const handleEdit = () => {
+    const handleSave = async (idx, id) => {
+        // get value from editing table data value
+        const name = document.getElementById(`equipmentType-${id}name`).innerText;
+        const description = document.getElementById(`equipmentType-${id}description`).innerText;
 
+        // update database doc
+        await updateDoc(doc(firestore, "equipmentTypes", id), {
+            name: name,
+            description: description,
+        });
+
+        const mutatedEditing = editing.map((element, index) => {
+            if (index === idx) return false;
+            else return element;
+        });
+        setEditing(mutatedEditing);
     }
 
-    const handleDelete = () => {
-        let rows = document.getElementsByTagName("td");
-        let dataArr = [];
-        for (let i = 0; i < rows.length; i++) {
-            dataArr.push(rows[i].innerText);
-        }
-        dataArr = dataArr.filter(e => e !== "" && e !== "Edit\nDelete\nCancel");
-
-        const equipmentTypeDataObj = [];
-        for (let i = 0; i < dataArr.length; i += 2) {
-            equipmentTypeDataObj.push({
-                name: dataArr[i],
-                description: dataArr[i + 1],
-            })
-        }
-        console.log(equipmentTypeDataObj);
+    const handleDelete = async id => {
+        setEquipmentTypesData(equipmentTypesData.filter(equipmentTypesData => equipmentTypesData.id !== id));
+        await deleteDoc(doc(firestore, "equipmentTypes", id));
     }
 
     const getEquipmentTypesData = async () => {
@@ -49,42 +44,23 @@ const EquipmentType = () => {
         const equipmentTypesQuery = query(equipmentTypesCollection);
         const querySnapshot = await getDocs(equipmentTypesQuery);
         setEquipmentTypesData([]);
+        setEditing(Array(querySnapshot.size).fill(false));
 
         querySnapshot.forEach(doc => {
             let data = doc.data();
             setEquipmentTypesData(curr => [...curr, {
                 id: doc.id,
-                render: <tr key={doc.id}>
-                    <td className={"equipmentType-tableElement"}>{}</td>
-                    <td className={"equipmentType-tableElement"}>{data.name}</td>
-                    <td className={"equipmentType-tableElement"}>{data.description}</td>
-                    <td className={"equipmentType-tableElement"}>
-                        <div className={"equipmentType-dropdownDiv"}>
-                            <button onClick={dropdown} className={"equipmentType-dropdownButton"}
-                                    id={"equipmentType-dropdownButtonID"}>
-                                <img src={"/triangle-right.svg"} alt={"Right arrow used to create dropdown menu."}
-                                     id={"equipmentType-tableLinkArrow"}>
-                                </img>
-                            </button>
-                            <div id={"myDropdown"} className={"dropdown-content"}>
-                                <a className={"equipmentType-dropdownButton"} onClick={handleEdit}>Edit</a>
-                                <a className={"equipmentType-dropdownButton"} onClick={handleDelete}>Delete</a>
-                                <button className={"equipmentType-dropdownButton"}
-                                        id={"equipmentType-dropdownCancel"}>Cancel
-                                </button>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
+                name: data.name,
+                description: data.description,
             }]);
         });
     }
 
     useEffect(() => {
+        setEquipmentTypesData([]);
         getEquipmentTypesData().then(() => {
             setLoaded(true);
         });
-        // eslint-disable-next-line
     }, [loaded]);
 
     return (<div id={"equipmentType-body"}>
@@ -99,7 +75,6 @@ const EquipmentType = () => {
                 </img>
             </a>
         </div>
-
         {loaded ? <div id={"equipmentType-data"}>
             <table className={"equipmentType-tableElement"} id={"equipmentType-table"}>
                 <tbody>
@@ -109,7 +84,35 @@ const EquipmentType = () => {
                     <th className={"equipmentType-tableHeading"}>Description</th>
                     <th className={"equipmentType-tableHeading"}></th>
                 </tr>
-                {equipmentTypesData.map(equipmentType => equipmentType.render)}
+                {equipmentTypesData.map((equipmentType, index) =>
+                    <tr key={equipmentType.id}>
+                        <td className={"equipmentType-tableElement"}>{index + 1}</td>
+                        <td className={"equipmentType-tableElement"} contentEditable={editing[index]}
+                            suppressContentEditableWarning={true}
+                            id={`equipmentType-${equipmentType.id}name`}>{equipmentType.name}</td>
+                        <td className={"equipmentType-tableElement"} contentEditable={editing[index]}
+                            suppressContentEditableWarning={true}
+                            id={`equipmentType-${equipmentType.id}description`}>{equipmentType.description}</td>
+                        <td className={"equipmentType-tableElement"}>
+                            <div id={"equipmentType-dropdown"}>
+                                <button id={"equipmentType-tableArrowButton"}>
+                                    <img src={"/triangle-right.svg"}
+                                         alt={"Right arrow used to create dropdown menu."}
+                                         id={"equipmentType-tableArrow"}/>
+                                </button>
+                                <div id={"equipmentType-dropdownMenu"}>
+                                    {editing[index] ?
+                                        <button onClick={() => handleSave(index, equipmentType.id)}
+                                                className={"equipmentType-dropdownButton"}>Save</button>
+                                        : <button onClick={() => handleEdit(index)}
+                                                  className={"equipmentType-dropdownButton"}>Edit</button>}
+                                    <button onClick={() => handleDelete(equipmentType.id)}
+                                            className={"equipmentType-dropdownButton"}>Delete</button>
+                                </div>
+                            </div>
+
+                        </td>
+                    </tr>)}
                 </tbody>
             </table>
         </div> : <p className={"loading"}>Loading...</p>}
