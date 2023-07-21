dacb13dfdb667aa441a28e5ed4cf7e3ad0f16522
Benjamin Nguyen <pg.benng@gmail.com>
2023-07-21 14:40:12 -0700

    boilerplate + route setup

---
+++ SCEM Web/src/routes/report.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/report.css';
+
+const Report = () => {
+    return (
+        <div id={"report-page"}>
+            Report
+        </div>
+    );
+}
+
+export default Report;
