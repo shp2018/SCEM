8a9d5619c44f466083df64b228aa78c99ef3b0b0
tomho5 <tungquangho@gmail.com>
2023-07-25 00:29:39 +0700

    fix tool alerts page

--- SCEM Web/src/routes/createToolAlerts.js
+++ SCEM Web/src/routes/createToolAlerts.js
@@ -1,111 +1,135 @@
-import React, {useEffect, useState} from "react";
-import {firestore} from "../firebase";
-import {doc, setDoc, collection, getDocs, query} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { firestore } from "../firebase";
+import {
+  doc,
+  setDoc,
+  collection,
+  getDocs,
+  query,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
 import "../css/createToolAlerts.css";
 
 function CreateToolAlerts() {
-    const [equipment, setEquipment] = useState("");
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
-    const [equipmentNames, setEquipmentNames] = useState([]);
-    const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [equipment, setEquipment] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [equipmentNames, setEquipmentNames] = useState([]);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
-    useEffect(() => {
-        // Fetch equipment names from Firestore collection
-        const fetchEquipmentNames = async () => {
-            const equipmentCollection = collection(firestore, "equipmentManagement");
-            const equipmentQuery = query(equipmentCollection);
-            const equipmentSnapshot = await getDocs(equipmentQuery);
-            const names = equipmentSnapshot.docs.map((doc) => doc.data().equipmentName);
-            setEquipmentNames(names);
-        };
-        fetchEquipmentNames();
-    }, []);
+  useEffect(() => {
+    // Fetch equipment names from Firestore collection
+    const fetchEquipmentNames = async () => {
+      const equipmentCollection = collection(firestore, "equipmentManagement");
+      const equipmentQuery = query(equipmentCollection);
+      const equipmentSnapshot = await getDocs(equipmentQuery);
+      const names = equipmentSnapshot.docs.map((doc) => doc.data().equipmentName);
+      setEquipmentNames(names);
+    };
+    fetchEquipmentNames();
+  }, []);
 
-    const handleSave = async e => {
-        e.preventDefault();
+  const handleButtonClick = (e) => {
+    e.preventDefault(); // Prevent form submission on button click
+    setIsButtonClicked(!isButtonClicked);
+  };
 
-        const toolAlertsDoc = doc(collection(firestore, "toolAlerts"));
+  const handleSave = async (e) => {
+    e.preventDefault();
 
-        let data = {
-            name: name,
-            description: description,
-            equipment: equipment,
-            active: isButtonClicked,
-        };
+    const toolAlertsDoc = doc(firestore, "toolAlerts", name);
+    const toolAlertsData = await getDoc(toolAlertsDoc);
 
-        await setDoc(toolAlertsDoc, data);
-        alert("Tool alerts updated.");
-        window.location.replace("/toolAlerts");
+    let data = {
+      name: name,
+      description: description,
+      equipment: equipment,
+      active: isButtonClicked,
     };
 
-    const handleButtonClick = () => {
-        setIsButtonClicked(!isButtonClicked);
-    };
+    if (toolAlertsData.exists()) {
+      await updateDoc(toolAlertsDoc, data);
+      alert("Existing tool alerts data has been updated.");
+    } else {
+      await setDoc(toolAlertsDoc, data);
+      alert("New tool alerts data has been created.");
+    }
 
-    return (
-        <div id={"createToolAlerts-page"}>
-            <div id={"createToolAlerts-header"}>
-                <div id={"createToolAlerts-backButton"}>
-                    <a href={"/toolAlerts"} className={"arrow left"}></a>
-                </div>
-                <div id={"createToolAlerts-createToolAlertsText"}>
-                    <h3>Create Alert</h3>
-                </div>
-            </div>
-            <br/>
-            <form id="createToolAlerts-form" onSubmit={handleSave}>
-                <div>
-                    <div id={"createToolAlerts-inputBoxes"}>
-                        <label id="createToolAlerts-Label">Equipment</label>
-                        <select
-                            className="createToolAlerts-Input"
-                            id={"createToolAlerts-equipmentInput"}
-                            value={equipment}
-                            onChange={(e) => {
-                                setEquipment(e.target.value);
-                            }}>
-                            <option value="">Select equipment name</option>
-                            {equipmentNames.map((equipmentName, index) => (
-                                <option key={index} value={equipmentName}>
-                                    {equipmentName}
-                                </option>
-                            ))}
-                        </select>
-                        <br/>
+    window.location.replace("/tool/alerts");
+  };
 
-                        <label id="createToolAlerts-Label">Name</label>
-                        <input
-                            className="createToolAlerts-Input"
-                            type="text"
-                            onChange={(e) => {
-                                setName(e.target.value);
-                            }}/>
-                        <br/>
-
-                        <label id="createToolAlerts-Label">Description</label>
-                        <input
-                            className="createToolAlerts-Input"
-                            type="text"
-                            onChange={(e) => {
-                                setDescription(e.target.value);
-                            }}/>
-
-                        <p>Set Alert Active Status</p>
-                        <button
-                            onClick={handleButtonClick}
-                            className={isButtonClicked ? "activeButton" : "inactiveButton"}>
-                            {isButtonClicked ? "On" : "Off"}
-                        </button>
-                    </div>
-                </div>
-
-                <button id="createToolAlerts-createToolAlertsbutton" type="submit" onClick={handleSave}>
-                    Update
-                </button>
-            </form>
+  return (
+    <div id={"createToolAlerts-page"}>
+      <div id={"createToolAlerts-header"}>
+        <div id={"createToolAlerts-backButton"}>
+          <a href={"/tool/alerts"} className={"arrow left"}></a>
         </div>
-    );
+        <div id={"createToolAlerts-createToolAlertsText"}>
+          <h3>Create Alert</h3>
+        </div>
+      </div>
+      <br />
+      <form id="createToolAlerts-form">
+        <div>
+          <div id={"createToolAlerts-inputBoxes"}>
+            <label id="createToolAlerts-Label">Equipment</label>
+            <select
+              className="createToolAlerts-Input"
+              id={"createToolAlerts-equipmentInput"}
+              value={equipment}
+              onChange={(e) => {
+                setEquipment(e.target.value);
+              }}
+            >
+              <option value="">Select equipment name</option>
+              {equipmentNames.map((equipmentName, index) => (
+                <option key={index} value={equipmentName}>
+                  {equipmentName}
+                </option>
+              ))}
+            </select>
+            <br />
+
+            <label id="createToolAlerts-Label">Name</label>
+            <input
+              className="createToolAlerts-Input"
+              type="text"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <br />
+
+            <label id="createToolAlerts-Label">Description</label>
+            <input
+              className="createToolAlerts-Input"
+              type="text"
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+
+            <p>Set Alert Active Status</p>
+            <button
+              onClick={handleButtonClick}
+              className={isButtonClicked ? "activeButton" : "inactiveButton"}
+            >
+              {isButtonClicked ? "On" : "Off"}
+            </button>
+          </div>
+        </div>
+
+        <button
+          id="createToolAlerts-createToolAlertsbutton"
+          type="submit"
+          onClick={handleSave} // Call handleSave explicitly when Update button is clicked
+        >
+          Update
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateToolAlerts;
--- SCEM Web/src/routes/toolAlerts.js
+++ SCEM Web/src/routes/toolAlerts.js
@@ -1,77 +1,211 @@
-import React, {useEffect, useState} from "react";
-import {collection, query, getDocs} from "firebase/firestore";
-import {firestore} from "../firebase";
+import React, { useState, useEffect } from 'react';
+import { getDocs, query, collection, doc, deleteDoc, updateDoc } from "firebase/firestore";
+import { firestore } from "../firebase";
 import "../css/toolAlerts.css";
 
 function ToolAlerts() {
-    const [alerts, setAlerts] = useState([]);
-    const alertsRef = collection(firestore, "toolAlerts");
+  const [editing, setEditing] = useState([]);
+  const [toolAlertsData, setToolAlertsData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [updatedData, setUpdatedData] = useState({});
 
-    async function getAlertsData() {
-        const alertsQuery = query(alertsRef);
-        const querySnapshot = await getDocs(alertsQuery);
-        setAlerts([]);
+  const handleEdit = (idx, name) => {
+    const mutatedEditing = editing.map((element, index) => {
+      if (index === idx) return true;
+      else return element;
+    });
+    setEditing(mutatedEditing);
 
-        querySnapshot.forEach((doc) => {
-            let data = doc.data();
-            const activeStatus = data.active ? "On" : "Off";
+    // Set the initial updated values for the entry being edited
+    const toolAlerts = toolAlertsData[idx];
+    setUpdatedData({
+      ...updatedData,
+      [name]: {
+        name: toolAlerts.name,
+        equipment: toolAlerts.equipment,
+        description: toolAlerts.description,
+        active: toolAlerts.active,
+      },
+    });
+  };
 
-            setAlerts((curr) => [
-                ...curr,
-                <tr key={`${doc.id}`}>
-                    <td>{}</td>
-                    <td>{data.name}</td>
-                    <td>{data.description}</td>
-                    <td>{activeStatus}</td>
-                    <td id={"toolAlerts-linkArrowBox"}>
-                        <a href={`/`}>
-                            <img
-                                src={"/triangle-right.svg"}
-                                alt={"Right arrow used to redirect user to item link."}
-                                id={"toolAlerts-tableLinkArrow"}
-                            ></img>
-                        </a>
-                    </td>
-                </tr>,
-            ]);
-        });
+  const handleSave = async (idx, name) => {
+    const updatedToolAlerts = updatedData[name];
+    if (updatedToolAlerts) {
+      // Update the database doc
+      await updateDoc(doc(firestore, 'toolAlerts', name), {
+        name: updatedToolAlerts.name,
+        equipment: updatedToolAlerts.equipment,
+        description: updatedToolAlerts.description,
+        active: updatedToolAlerts.active,
+      });
+
+      const mutatedEditing = editing.map((element, index) => {
+        if (index === idx) return false;
+        else return element;
+      });
+      setEditing(mutatedEditing);
     }
+  };
 
-    useEffect(() => {
-        getAlertsData();
-        // eslint-disable-next-line
-    }, []);
+  const handleDelete = async (name) => {
+    setToolAlertsData(toolAlertsData.filter((toolAlertsData) => toolAlertsData.name !== name));
+    await deleteDoc(doc(firestore, 'toolAlerts', name));
+  };
 
-    return (
-        <div id={"toolAlerts-page"}>
-            <div id={"toolAlerts-header"}>
-                <div id={"toolAlerts-backButton"}>
-                    <a href={"/"} className={"arrow left"}></a>
-                </div>
-                <div id={"toolAlerts-toolAlertsText"}>
-                    <h3>Alerts</h3>
-                </div>
-                <a href="/toolAlerts/create">
-                    <img
-                        src="/locationAdd.png"
-                        id="toolAlerts-addButton"
-                        alt="add equipment button"
-                    ></img>
-                </a>
-            </div>
-            <br></br>
-            <table id="alerts">
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Active</th>
-                    <th></th>
-                </tr>
-                {alerts}
-            </table>
+  const getToolAlertsData = async () => {
+    const toolAlertsCollection = collection(firestore, 'toolAlerts');
+    const toolAlertsQuery = query(toolAlertsCollection);
+    const querySnapshot = await getDocs(toolAlertsQuery);
+    setToolAlertsData([]);
+    setEditing(Array(querySnapshot.size).fill(false));
+    setUpdatedData({});
+
+    querySnapshot.forEach((doc) => {
+      let data = doc.data();
+      setToolAlertsData((curr) => [
+        ...curr,
+        {
+          name: doc.id,
+          equipment: data.equipment,
+          description: data.description,
+          active: data.active,
+        },
+      ]);
+    });
+  };
+
+  useEffect(() => {
+    setToolAlertsData([]);
+    getToolAlertsData().then(() => {
+      setLoaded(true);
+    });
+  }, []);
+
+  return (
+    <div id={'toolAlerts-body'}>
+      <div id={'toolAlerts-header'}>
+        <div id={'toolAlerts-backButtonDiv'}>
+          <a href={'/'} className={'arrow left'}></a>
         </div>
-    );
+        <h3 id={'toolAlerts-titleText'}>Alerts</h3>
+        <a href={'/tool/alerts/create'}>
+          <img src={'/locationAdd.png'} id={'toolAlerts-addButton'} alt={'Add equipment type button'}></img>
+        </a>
+      </div>
+      {loaded ? (
+        <div id={'toolAlerts-data'}>
+          <table className={'toolAlerts-tableElement'} id={'toolAlerts-table'}>
+            <tbody>
+              <tr>
+                <th className={'toolAlerts-tableHeading'}>#</th>
+                <th className={'toolAlerts-tableHeading'}>Name</th>
+                <th className={'toolAlerts-tableHeading'}>Description</th>
+                <th className={'toolAlerts-tableHeading'}>Active</th>
+                <th className={'toolAlerts-tableHeading'}></th>
+              </tr>
+              {toolAlertsData.map((toolAlerts, index) => (
+                <tr key={toolAlerts.name}>
+                  <td className={'toolAlerts-tableElement'}>{index + 1}</td>
+                  <td
+                    className={'toolAlerts-tableElement'}
+                    contentEditable={editing[index]}
+                    suppressContentEditableWarning={true}
+                    id={`toolAlerts-${toolAlerts.name}name`}
+                    onInput={(e) => {
+                      const updatedValue = e.target.textContent;
+                      setUpdatedData({
+                        ...updatedData,
+                        [toolAlerts.name]: {
+                          ...updatedData[toolAlerts.name],
+                          active: updatedValue.toLowerCase() === 'on' ? true : false,
+                        },
+                      });
+                    }}
+                  >
+                    {toolAlerts.name}
+                  </td>
+                  <td
+                    className={'toolAlerts-tableElement'}
+                    contentEditable={editing[index]}
+                    suppressContentEditableWarning={true}
+                    id={`toolAlerts-${toolAlerts.name}description`}
+                    onInput={(e) => {
+                      const updatedValue = e.target.textContent;
+                      setUpdatedData({
+                        ...updatedData,
+                        [toolAlerts.name]: {
+                          ...updatedData[toolAlerts.name],
+                          description: updatedValue,
+                        },
+                      });
+                    }}
+                  >
+                    {toolAlerts.description}
+                  </td>
+                  <td
+                    className={'toolAlerts-tableElement'}
+                    contentEditable={editing[index]}
+                    suppressContentEditableWarning={true}
+                    id={`toolAlerts-${toolAlerts.name}active`}
+                    onInput={(e) => {
+                      const updatedValue = e.target.textContent.toLowerCase();
+                      setUpdatedData({
+                        ...updatedData,
+                        [toolAlerts.name]: {
+                          ...updatedData[toolAlerts.name],
+                          active: updatedValue === 'on' ? true : false,
+                        },
+                      });
+                    }}
+                  >
+                    {toolAlerts.active ? 'On' : 'Off'}
+                  </td>
+
+                  <td className={'toolAlerts-tableElement'}>
+                    <div id={'toolAlerts-dropdown'}>
+                      <button id={'toolAlerts-tableArrowButton'}>
+                        <img
+                          src={'/triangle-right.svg'}
+                          alt={'Right arrow used to create dropdown menu.'}
+                          id={'toolAlerts-tableArrow'}
+                        />
+                      </button>
+                      <div id={'toolAlerts-dropdownMenu'}>
+                        {editing[index] ? (
+                          <button
+                            onClick={() => handleSave(index, toolAlerts.name)}
+                            className={'toolAlerts-dropdownButton'}
+                          >
+                            Save
+                          </button>
+                        ) : (
+                          <button
+                            onClick={() => handleEdit(index, toolAlerts.name)}
+                            className={'toolAlerts-dropdownButton'}
+                          >
+                            Edit
+                          </button>
+                        )}
+                        <button
+                          onClick={() => handleDelete(toolAlerts.name)}
+                          className={'toolAlerts-dropdownButton'}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <p className={'loading'}>Loading...</p>
+      )}
+    </div>
+  );
 }
 
 export default ToolAlerts;
