c02c247bc0f1c0ddfe3eeb17c7b1d535a4e6d20e
Benjamin Nguyen <pg.benng@gmail.com>
2023-07-24 16:23:55 -0700

    boilerplate - 30

---
+++ SCEM Web/src/routes/user.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import '../css/user.css';
+
+const User = () => {
+    return (
+      <div id={"user-page"}>
+          User
+      </div>
+    );
+}
+
+export default User;
