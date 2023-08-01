17cc3ed9d018b1233b5d7b0e6330f5a0e8945855
Benjamin Nguyen <pg.benng@gmail.com>
2023-07-31 22:23:02 -0700

    temp

--- SCEM Web/src/routes/manufactureManagement.js
+++ SCEM Web/src/routes/manufactureManagement.js
@@ -1,9 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import '../css/manufactureManagement.css';
+import {collection, query, where, getDocs} from "firebase/firestore";
+import {firestore} from "../firebase";
 
 const ManufactureManagement = () => {
-    const handleSearch = e => {
+    const [searchResults, setSearchResults] = useState([]);
+    const [name, setName] = useState("");
+
+    const handleSearch = async e => {
         e.preventDefault();
+        setSearchResults([]);
+        let results = [];
+
+        const ref = collection(firestore, "manufacture");
+        const searchTerms = name.toLowerCase().split(" ");
+        const searchQuery = query(ref, where("searchTerms", "array-contains-any", searchTerms));
+
+        const querySnapshot = await getDocs(searchQuery);
+        querySnapshot.forEach(doc => {
+            results.push(doc);
+        });
+
+        setSearchResults(results);
+    }
+
+    const redirectToCreate = () => {
+        window.location.href = "/manufactureManagement/create"
     }
 
     return (
@@ -15,13 +37,45 @@ const ManufactureManagement = () => {
                 <h3 id={'manufactureManagement-titleText'}>Manufacture Management</h3>
             </div>
             <form id={"manufactureManagement-form"} onSubmit={handleSearch}>
-                <label>Name</label>
+                <label id={"manufactureManagement-formName"}>Name</label>
                 <br></br>
-                <input/>
+                <input id={"manufactureManagement-searchInput"} onChange={(e) => setName(e.target.value)}/>
                 <br></br>
-                <button id={"manufactureManagement-button"} type={"submit"}>Search</button>
+                <button id={"manufactureManagement-searchButton"} type={"submit"}>Search</button>
             </form>
-            <a href={"/manufactureManagement/create"}>Create</a>
+            <div id={"manufactureManagement-createButtonDiv"}>
+                <button id={"manufactureManagement-createButton"} onClick={redirectToCreate}>Create New
+                </button>
+                <img src={"/locationAdd.png"} id={"manufactureManagement-plusIcon"} alt={"Plus icon"}>
+                </img>
+            </div>
+            <table id={"manufactureManagement-table"} className={"manufactureManagement-tableElement"}>
+                <tbody>
+                <tr>
+                    <th className={"manufactureManagement-tableHeading"}>#</th>
+                    <th className={"manufactureManagement-tableHeading"}>Name</th>
+                    <th className={"manufactureManagement-tableHeading"}>Email</th>
+                    <th className={"manufactureManagement-tableHeading"}>Phone</th>
+                    <th className={"manufactureManagement-tableHeading"}>Tax ID</th>
+                    <th className={"manufactureManagement-tableHeading"}>Model</th>
+                    <th className={"manufactureManagement-tableHeading"}></th>
+                </tr>
+                {searchResults.map((doc, index) =>
+                    <tr key={doc.id}>
+                        <td className={"manufactureManagement-tableElement"}>{index + 1}</td>
+                        <td className={"manufactureManagement-tableElement"}>{doc.data().name}</td>
+                        <td className={"manufactureManagement-tableElement"}>{doc.data().email}</td>
+                        <td className={"manufactureManagement-tableElement"}>{doc.data().phone}</td>
+                        <td className={"manufactureManagement-tableElement"}>{doc.data().taxID}</td>
+                        <td className={"manufactureManagement-tableElement"}>{doc.data().model}</td>
+                        <td className={"manufactureManagement-tableElement"}></td>
+                    </tr>)}
+                </tbody>
+            </table>
+            <div id={"manufactureManagement-pageNavigation"}>
+                <button onClick={() => null}>Prev</button>
+                <button onClick={() => null}>Next</button>
+            </div>
         </div>
     );
 }
