487854042e33cc9466551f8998882a671480d7c8
Benjamin Nguyen <pg.benng@gmail.com>
2023-08-10 15:23:44 -0700

    final - 35

--- SCEM Web/src/routes/modelManagement.js
+++ SCEM Web/src/routes/modelManagement.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import '../css/modelManagement.css';
-import { collection, query, where, getDocs, updateDoc, doc } from "firebase/firestore";
-import { firestore } from "../firebase";
+import {collection, query, where, getDocs, updateDoc, doc} from "firebase/firestore";
+import {firestore} from "../firebase";
 import PageNumber from "../components/pageNumber";
 
 const ModelManagement = () => {
@@ -22,6 +22,7 @@ const ModelManagement = () => {
             const names = querySnapshot.docs.map(doc => doc.data().mName);
             setManufactureNames(names);
         }
+
         fetchManufactureNames();
     }, []);
 
@@ -50,7 +51,7 @@ const ModelManagement = () => {
         let pageNumbers = Math.ceil(searchResults.length / MAX_RESULTS);
         let res = [];
         for (let i = 0; i < pageNumbers; i++) {
-            res.push(<PageNumber key={i} num={i + 1} handler={() => setCurrentPageNumber(i + 1)} />);
+            res.push(<PageNumber key={i} num={i + 1} handler={() => setCurrentPageNumber(i + 1)}/>);
         }
         setPageNumbers(res);
         setCurrentPageNumber(1);
@@ -75,11 +76,10 @@ const ModelManagement = () => {
             return mutatedEditing;
         });
 
-       
         if (!editedModels[idx]) {
             setEditedModels(prevEditedModels => ({
                 ...prevEditedModels,
-                [idx]: { ...searchResults[idx].data() },
+                [idx]: {...searchResults[idx].data()},
             }));
         }
     }
@@ -87,24 +87,20 @@ const ModelManagement = () => {
     const handleSave = async (idx, id) => {
         const updatedModel = editedModels[idx];
         if (updatedModel) {
-           
             await updateDoc(doc(firestore, "model", id), updatedModel);
 
-           
             setEditedModels(prevEditedModels => {
-                const editedModelCopy = { ...prevEditedModels };
+                const editedModelCopy = {...prevEditedModels};
                 delete editedModelCopy[idx];
                 return editedModelCopy;
             });
 
-          
             setEditing(prevEditing => {
                 const mutatedEditing = [...prevEditing];
                 mutatedEditing[idx] = false;
                 return mutatedEditing;
             });
 
-           
             window.location.reload();
         }
     }
@@ -118,106 +114,111 @@ const ModelManagement = () => {
                 <h3 id={'modelManagement-titleText'}>Model Management</h3>
             </div>
             <form id={"modelManagement-form"} onSubmit={handleSearch}>
-    <div className="search-input-container">
-        <label htmlFor={"modelManagement-searchInput"}>Model</label>
-        <input
-            id={"modelManagement-searchInput"}
-            onChange={(e) => setModel(e.target.value)}
-        />
-    </div>
-    <div className="search-input-container">
-        <label htmlFor={"modelManagement-manufactureSelect"}>Manufacture</label>
-        <select
-            id={"modelManagement-manufactureSelect"}
-            value={manufacture}
-            onChange={(e) => setManufacture(e.target.value)}
-        >
-            <option value="">All Manufactures</option>
-            {manufactureNames.map(name => (
-                <option key={name} value={name}>{name}</option>
-            ))}
-        </select>
-    </div>
-    <button id={"modelManagement-searchButton"} type={"submit"}>
-        Search
-    </button>
-</form>
-
+                <div id={"modelManagement-inputDivModel"}>
+                    <label htmlFor={"modelManagement-searchInput"} className={"modelManagement-label"}>Model</label>
+                    <br></br>
+                    <input
+                        id={"modelManagement-searchInput"}
+                        onChange={(e) => setModel(e.target.value)}/>
+                </div>
+                <div id={"modelManagement-inputDivManufacture"}>
+                    <label htmlFor={"modelManagement-manufactureSelect"}
+                           className={"modelManagement-label"}>Manufacture</label>
+                    <br></br>
+                    <select
+                        id={"modelManagement-manufactureSelect"}
+                        value={manufacture}
+                        onChange={(e) => setManufacture(e.target.value)}>
+                        <option value="">All Manufactures</option>
+                        {manufactureNames.map(name => (
+                            <option key={name} value={name}>{name}</option>
+                        ))}
+                    </select>
+                </div>
+                <button id={"modelManagement-searchButton"} type={"submit"}>
+                    Search
+                </button>
+            </form>
 
             <div id={"modelManagement-createButtonDiv"}>
-                <button id={"modelManagement-createButton"} onClick={() => window.location.href = "/modelManagement/create"}>Create New
-                    <img src={"/locationAdd.png"} id={"modelManagement-plusIcon"} alt={"Plus icon"} />
+                <button id={"modelManagement-createButton"}
+                        onClick={() => window.location.href = "/modelManagement/create"}>Create New
+                    <img src={"/locationAdd.png"} id={"modelManagement-plusIcon"} alt={"Plus icon"}/>
                 </button>
             </div>
             <table id={"modelManagement-table"} className={"modelManagement-tableElement"}>
-    <tbody>
-    <tr>
-    <th className={"modelManagement-tableHeading"}>#</th>
-    <th className={"modelManagement-tableHeading"}>Model</th>
-    <th className={"modelManagement-tableHeading"}>Description</th>
-    <th className={"modelManagement-tableHeading"}>Equipment Type</th>
-    <th className={"modelManagement-tableHeading"}>Manufacture</th>
-    <th className={"modelManagement-tableHeading"}></th>
-</tr>
-</tbody>
-
-<tbody>
-    {searchResults.slice((currentPageNumber - 1) * MAX_RESULTS, currentPageNumber * MAX_RESULTS).map((doc, index) =>
-        <tr key={doc.id}>
-            <td className={"modelManagement-tableElement"}>{index + 1}</td> 
-            <td
-                className={"modelManagement-tableElement"}
-                id={`modelManagement-${doc.id}modModel`}
-                contentEditable={editing[index]}
-                suppressContentEditableWarning={true}
-                onBlur={e => {
-                    const editedModelCopy = { ...editedModels };
-                    editedModelCopy[index] = { ...editedModelCopy[index], modModel: e.target.innerText };
-                    setEditedModels(editedModelCopy);
-                }}
-            >
-                {editedModels[index]?.modModel || doc.data().modModel}
-            </td>
-            <td
-                className={"modelManagement-tableElement"}
-                id={`modelManagement-${doc.id}modDescription`}
-                contentEditable={editing[index]}
-                suppressContentEditableWarning={true}
-                onBlur={e => {
-                    const editedModelCopy = { ...editedModels };
-                    editedModelCopy[index] = { ...editedModelCopy[index], modDescription: e.target.innerText };
-                    setEditedModels(editedModelCopy);
-                }}
-            >
-                {editedModels[index]?.modDescription || doc.data().modDescription}
-            </td>
-            <td
-                className={"modelManagement-tableElement"}
-                id={`modelManagement-${doc.id}modEquipmentType`}
-                contentEditable={editing[index]}
-                suppressContentEditableWarning={true}
-                onBlur={e => {
-                    const editedModelCopy = { ...editedModels };
-                    editedModelCopy[index] = { ...editedModelCopy[index], modEquipmentType: e.target.innerText };
-                    setEditedModels(editedModelCopy);
-                }}
-            >
-                {editedModels[index]?.modEquipmentType || doc.data().modEquipmentType}
-            </td>
-            <td className={"modelManagement-tableElement"} id={`modelManagement-${doc.id}modName`}>
-                {editedModels[index]?.modName || doc.data().modName} 
-            </td>
-            <td className={"modelManagement-tableElement"}>
-                <div className="modelManagement-tableButtonContainer">
-                    {!editing[index] ?
-                        <button onClick={() => handleEdit(index)} className={"modelManagement-editButton"}>Edit</button>
-                        : <button onClick={() => handleSave(index, doc.id)} className={"modelManagement-saveButton"}>Save</button>}
-                </div>
-            </td>
-        </tr>
-    )}
-</tbody>
-</table>
+                <tbody>
+                <tr>
+                    <th className={"modelManagement-tableHeading"}>#</th>
+                    <th className={"modelManagement-tableHeading"}>Model</th>
+                    <th className={"modelManagement-tableHeading"}>Description</th>
+                    <th className={"modelManagement-tableHeading"}>Equipment Type</th>
+                    <th className={"modelManagement-tableHeading"}>Manufacture</th>
+                    <th className={"modelManagement-tableHeading"}></th>
+                </tr>
+                </tbody>
+                <tbody>
+                {searchResults.slice((currentPageNumber - 1) * MAX_RESULTS, currentPageNumber * MAX_RESULTS).map((doc, index) =>
+                    <tr key={doc.id}>
+                        <td className={"modelManagement-tableElement"}>{index + 1}</td>
+                        <td
+                            className={"modelManagement-tableElement"}
+                            id={`modelManagement-${doc.id}modModel`}
+                            contentEditable={editing[index]}
+                            suppressContentEditableWarning={true}
+                            onBlur={e => {
+                                const editedModelCopy = {...editedModels};
+                                editedModelCopy[index] = {...editedModelCopy[index], modModel: e.target.innerText};
+                                setEditedModels(editedModelCopy);
+                            }}>
+                            {editedModels[index]?.modModel || doc.data().modModel}
+                        </td>
+                        <td
+                            className={"modelManagement-tableElement"}
+                            id={`modelManagement-${doc.id}modDescription`}
+                            contentEditable={editing[index]}
+                            suppressContentEditableWarning={true}
+                            onBlur={e => {
+                                const editedModelCopy = {...editedModels};
+                                editedModelCopy[index] = {
+                                    ...editedModelCopy[index],
+                                    modDescription: e.target.innerText
+                                };
+                                setEditedModels(editedModelCopy);
+                            }}>
+                            {editedModels[index]?.modDescription || doc.data().modDescription}
+                        </td>
+                        <td
+                            className={"modelManagement-tableElement"}
+                            id={`modelManagement-${doc.id}modEquipmentType`}
+                            contentEditable={editing[index]}
+                            suppressContentEditableWarning={true}
+                            onBlur={e => {
+                                const editedModelCopy = {...editedModels};
+                                editedModelCopy[index] = {
+                                    ...editedModelCopy[index],
+                                    modEquipmentType: e.target.innerText
+                                };
+                                setEditedModels(editedModelCopy);
+                            }}>
+                            {editedModels[index]?.modEquipmentType || doc.data().modEquipmentType}
+                        </td>
+                        <td className={"modelManagement-tableElement"} id={`modelManagement-${doc.id}modName`}>
+                            {editedModels[index]?.modName || doc.data().modName}
+                        </td>
+                        <td className={"modelManagement-tableElement"}>
+                            <div className="modelManagement-tableButtonContainer">
+                                {!editing[index] ?
+                                    <button onClick={() => handleEdit(index)}
+                                            className={"modelManagement-editButton"}>Edit</button>
+                                    : <button onClick={() => handleSave(index, doc.id)}
+                                              className={"modelManagement-saveButton"}>Save</button>}
+                            </div>
+                        </td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
             <div id={"modelManagement-pageNavigation"}>
                 <button onClick={decrementPageNumber} id={"modelManagement-prev"}>Prev</button>
                 {pageNumbers}
